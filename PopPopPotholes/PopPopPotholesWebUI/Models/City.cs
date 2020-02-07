e15f36eb214d87412dc5aa63d2eeb16546b6f19e
Dotakiin <47369531+Dotakiin@users.noreply.github.com>
2020-02-07 13:00:45 -0600

    User experience

---
+++ PopPopPotholes/PopPopPotholesWebUI/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PopPopPotholesWebUI.Models
+{
+    public class City
+    {
+        public string CityName { get; set; }
+        public string CountyName { get; set; }
+        public string StateName { get; set; }
+        public string CountryName { get; set; }
+    }
+}
---
+++ PopPopPotholes/PopPopPotholesWebUI/Models/Issue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PopPopPotholesWebUI.Models
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        public DateTime IssueTime { get; set; }
+        public string IssueType { get; set; }
+        public string IssueSeverity { get; set; }
+        public int CityId { get; set; }
+        public string ImageLink { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string City { get; set; }
+        public string Summary { get; set; }
+    }
+}
